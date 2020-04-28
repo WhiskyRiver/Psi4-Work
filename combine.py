4bca1c91e985e022d8ef536b0ebd37bdf6e4d7d1
WhiskyRiver <joseph.hamill@live.com>
2020-04-27 17:01:03 -0700

    Combines outputs from Psi4 into one combined file
    The newly created file can be read by Avogadro (with a few errors) and Molden. In both cases, for some reason, the intensities don't load right, but the frequencies do.

--- combine.py
+++ combine.py
@@ -27,7 +27,32 @@ CombineContent = StructContent + FreqContent[1:len(FreqContent)-1] + IRContent
 with open('combined.molden', 'w') as CombineFile:
     for item in CombineContent:
         CombineFile.write("%s\n" % item)
-        
-with open('combined.molden', 'w') as CombineFile:
-    for item in CombineContent:
-        CombineFile.write("%s\n" % item)
+
+## This also creates a very basic multiuse geometry.xyz file with just coordinates
+# reopen and read geometry content
+GeometryContent = []
+line_after_trigger = -1
+trigger_line = []
+trigger_flag = False
+start_record = False
+stop_record = False
+with open ('h2o.out', 'rt') as IRFile:
+    for line in IRFile:
+        if 'Final (previous) structure:' in line:
+            trigger_flag = True
+        if trigger_flag == True:
+            line_after_trigger = line_after_trigger + 1
+            if line_after_trigger == 2:
+                start_record = True
+            if start_record == True:
+                if 'Saving final (previous) structure.' in line:
+                    stop_record == True
+                    break
+                if stop_record == False:
+                    GeometryContent.append(line.rstrip('\n'))
+                    
+## Write new Geomety file
+with open('geometry.xyz', 'w') as GeometryFile:
+    GeometryFile.write("%s\n\n" % len(GeometryContent))
+    for item in GeometryContent:
+        GeometryFile.write("%s\n" % item)
